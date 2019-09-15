479fde416ba4396c8072a1de25821f6a4e93ee98
Enzo Esperguin <enzoesper@hotmail.com>
2019-09-15 17:13:01 -0300

    handle the overfetching problem with solution #1

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import _ from "lodash";
+
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 export const fetchPosts = () => async dispatch => {
@@ -7,10 +9,12 @@ export const fetchPosts = () => async dispatch => {
 };
 
 // fetching one user at a time
-export const fetchUser = id => {
-  return async dispatch => {
-    const response = await jsonPlaceholder.get(`/users/${id}`);
-
-    dispatch({ type: "FETCH_USER", payload: response.data });
-  };
+export const fetchUser = id => dispatch => {
+  _fetchUser(id, dispatch);
 };
+// memoizing the fetchUser for executing it only one time for each id of the users
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: "FETCH_USER", payload: response.data });
+});
